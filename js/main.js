3e42959511a3f54a827fcf5b38dc844a43f35b24
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2021-07-29 12:46:54 +0200

    styles and JS part1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+((d)=>{
+    // DOM
+    const time = d.getElementById('time'),
+    greetins=d.getElementById('greeting'),
+    name=d.getElementById('name'),
+    focus=d.getElementById('focus');
+    
+    //Time
+    function ShowRealTime() {
+        let today=new Date();
+        let hour=today.getHours();
+        let min=today.getMinutes();
+        let sec=today.getSeconds();      
+    }
+
+
+})(document)
+
