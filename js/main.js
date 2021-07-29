e9b1a2766e513e06764f1da0d48916887e0a2a89
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2021-07-29 13:36:32 +0200

    bg

--- js/main.js
+++ js/main.js
@@ -12,8 +12,42 @@
         let today=new Date();
         let hour=today.getHours();
         let min=today.getMinutes();
-        let sec=today.getSeconds();      
+        let sec=today.getSeconds(); 
+        
+           //Salida del tiempo
+        time.innerHTML=`${hour}<span>:</span>${min}<span>:</span>${sec}`;
+        setTimeout(ShowRealTime,1000);
     }
+    
+    //background-image
+
+    function setHeroImage() {
+        let today=new Date();
+        let hour=today.getHours();
+        if (hour<12) {
+            document.body.style.backgroundImage="url('../img/morning.jpg')";
+            greetins.textContent="Good Morning";
+
+            
+        } else if(hour<18) {
+            document.body.style.backgroundImage="url('../img/afternoon.jpg')";
+            greetins.textContent="Good Afternoon";
+            
+        }else{
+            document.body.backgroundImage="url('../img/night.jpg')";
+            greetins.textContent="Good evening";
+            document.body.style.color="white";
+
+        }
+        
+    }
+
+    //correr las funciones
+
+    ShowRealTime();
+    setHeroImage()
+
+ 
 
 
 })(document)
